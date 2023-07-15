8c4f21dfc0f580d3e78e7e17c69f50b5ad08e074
Sougata_Palit <91267208+Palit-S@users.noreply.github.com>
2023-07-15 23:21:06 +0530

    Update script.js
    Update string variables

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Engineer", "Designer", "Freelancer"],
+        strings: [ "Front-End Dev", "Software Engineer", "UI Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Engineer", "Designer", "Freelancer"],
+        strings: ["Front-End Dev", "Software Engineer", "UI Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
